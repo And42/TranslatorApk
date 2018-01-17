b236559eb556dd78c0db173751bf5e4c82752cf6
And42 <andrewskobcov@yandex.ru>
2018-01-17 21:40:05 +0300

    - Version updated to 1.4.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.2.0")]
+[assembly: AssemblyVersion("1.4.3.0")]
 [assembly: NeutralResourcesLanguage("ru")]
 
