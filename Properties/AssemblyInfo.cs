93ad2a738ef88b70903572d49b6e0f513db9811e
And42 <andrewskobcov@yandex.ru>
2018-03-11 11:39:04 +0300

    Version updated to 1.5.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.3.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
 [assembly: NeutralResourcesLanguage("ru")]
 
 
