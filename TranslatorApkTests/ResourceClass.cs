9a2ca3ab324ba1c16465ec8755e21885280d9df9
And42 <andrewskobcov@yandex.ru>
2018-07-04 19:19:58 +0300

    Removed unued classes and references

--- TranslatorApkTests/ResourceClass.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TranslatorApkTests
-{
-    public static class ResourceClass
-    {
-        public static readonly string TestFile = @"C:\My Documents\Visual Studio 2015\Projects\MyProjects\TranslatorApk\TranslatorApkTests\bin\Release\Tests\5 pic.apk";
-    }
-}
