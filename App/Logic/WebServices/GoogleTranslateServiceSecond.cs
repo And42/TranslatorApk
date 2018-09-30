8e3efff1d30ea3c7894899bc60a7bcc49c81fbbf
And42 <andrewskobcov@yandex.ru>
2018-09-30 12:58:34 +0300

    Changed source language for google translator

--- App/Logic/WebServices/GoogleTranslateServiceSecond.cs
+++ App/Logic/WebServices/GoogleTranslateServiceSecond.cs
@@ -8,7 +8,7 @@ namespace TranslatorApk.Logic.WebServices
     {
         public static string Translate(string text, string targetLanguage)
         {
-            string link = "http://" + $"translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl={targetLanguage}&dt=t&q={HttpUtility.UrlEncode(text)}";
+            string link = "http://" + $"translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl={targetLanguage}&dt=t&q={HttpUtility.UrlEncode(text)}";
             string downloaded = Utils.WebUtils.DownloadString(link, GlobalVariables.AppSettings.TranslationTimeout);
 
             var obj = JArray.Parse(downloaded);
